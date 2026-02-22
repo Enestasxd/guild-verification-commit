02650576ebaec5db29b90c2ac96ad21fdab60d5b
Enestasxd <85314282+Enestasxd@users.noreply.github.com>
2026-02-22 17:23:37 +0300

    Add multi-language support

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-02-22T14:23:36.216Z
-Commit: Resolve timezone issues in date handling
+Timestamp: 2026-02-22T14:23:37.182Z
+Commit: Add multi-language support
