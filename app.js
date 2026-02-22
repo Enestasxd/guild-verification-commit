89e961da22affd2b223ae1dee28ab2633f586ce3
Enestasxd <85314282+Enestasxd@users.noreply.github.com>
2026-02-22 17:24:20 +0300

    Fix memory leak in event handlers

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-02-22T14:24:11.384Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2026-02-22T14:24:19.850Z
+Commit: Fix memory leak in event handlers
