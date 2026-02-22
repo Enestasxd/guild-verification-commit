3c096e702121b99cd91fbc10731adaa69d73bb22
Enestasxd <85314282+Enestasxd@users.noreply.github.com>
2026-02-22 16:47:32 +0300

    Patch API endpoint error handling

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-02-22T13:47:15.869Z
-Commit: Fix broken links in documentation
+Timestamp: 2026-02-22T13:47:31.737Z
+Commit: Patch API endpoint error handling
