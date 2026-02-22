2f66fc16abd12fd2c6289e13bd520032e4351992
Enestasxd <85314282+Enestasxd@users.noreply.github.com>
2026-02-22 16:48:15 +0300

    Resolve timezone issues in date handling

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-02-22T13:48:10.069Z
-Commit: Implement user profile customization
+Timestamp: 2026-02-22T13:48:14.936Z
+Commit: Resolve timezone issues in date handling
