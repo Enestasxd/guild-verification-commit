831c7f0dfb8e22b3a4a3742ba24045c84018ad6e
Enestasxd <85314282+Enestasxd@users.noreply.github.com>
2026-02-22 17:24:30 +0300

    Resolve null pointer exception in user service

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-02-22T14:24:26.863Z
-Commit: Fix race condition in async operations
+Timestamp: 2026-02-22T14:24:29.610Z
+Commit: Resolve null pointer exception in user service
