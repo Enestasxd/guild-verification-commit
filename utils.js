08d95f8168bd3c21a5156eec5c69337eac1c3885
Enestasxd <85314282+Enestasxd@users.noreply.github.com>
2026-02-22 16:47:29 +0300

    Patch security vulnerability in authentication

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-02-22T13:47:14.453Z
-Commit: Resolve null pointer exception in user service
+Timestamp: 2026-02-22T13:47:29.167Z
+Commit: Patch security vulnerability in authentication
