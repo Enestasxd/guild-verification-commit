b194b23b745315fa5ab24f1b0d817f7cdb6c6814
Enestasxd <85314282+Enestasxd@users.noreply.github.com>
2026-02-22 16:47:23 +0300

    Fix CSS styling bug on mobile devices

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-02-22T13:47:20.231Z
-Commit: Introduce real-time notifications
+Timestamp: 2026-02-22T13:47:22.968Z
+Commit: Fix CSS styling bug on mobile devices
