cc8a154e832aea8039838644d76b172da1ca8997
Enestasxd <85314282+Enestasxd@users.noreply.github.com>
2026-02-22 17:24:16 +0300

    Patch API endpoint error handling

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-02-22T14:24:12.400Z
-Commit: Resolve timezone issues in date handling
+Timestamp: 2026-02-22T14:24:15.632Z
+Commit: Patch API endpoint error handling
